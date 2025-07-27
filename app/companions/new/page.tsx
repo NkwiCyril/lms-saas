34cb1e3c97db1ccf7ba4ee49c6fe415c115904a2
NkwiCyril <akinimbomnkwi@gmail.com>
2025-07-27 16:31:33 +0100

    Redirect to custom clerk sign in page for guest user

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,15 @@
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  
+  if (!userId) {
+    return redirect("/sign-in");
+  }
 
-
-const NewCompanion = () => {
   return (
     <div className="min-md:w-2/3 min-lg:w-1/3 flex flex-col mx-auto gap-4">
       <h2 className="text-2xl font-bold">Companion Builder</h2>
